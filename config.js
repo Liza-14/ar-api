642753b0fc93ae23f0ee6afea9b5734be1db5d2c
Yelyzaveta Malovichko <lizabvb57@gmail.com>
2023-05-13 17:08:31 +0300

    prod configuration

--- config.js
+++ config.js
@@ -4,13 +4,17 @@ function getConfigFileForEnvironment(env) {
     switch (env) {
         case 'local':
             return 'config.local.json';
-        case 'production':
+        case 'prod':
             return 'config.prod.json';
+        default:
+            return 'config.local.json';
     }
 }
 
-const configFilePath = getConfigFileForEnvironment(process.env.NODE_ENV);
+const env = process.argv.filter(x => x.startsWith('env='))[0]?.slice(4)
+const configFilePath = getConfigFileForEnvironment(env);
 const rawData = fs.readFileSync(configFilePath);
 const config = JSON.parse(rawData);
 
+console.log(config)
 export default config;
--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,13 @@
-import dotenv from "dotenv";
+import config from '../config.js';
 import pg from "pg";
 
-dotenv.config();
-
 const client = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-  })
+  user: config.DB_USER,
+  host: config.DB_HOST,
+  password: config.DB_PASS,
+  database: config.DB_NAME,
+  port: config.DB_PORT
+})
 
 client.connect()
 export default client;
